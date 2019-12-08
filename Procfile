4802ae2361bd5d5345c6c8ef42a5ee53092e5733
raifran1 <raifranlucas@outlook.com>
2019-12-07 22:13:06 -0300

    deploy on heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn web_blog.wsgi --log-file -
--- web_blog/settings.py
+++ web_blog/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+from dj_database_url import parse as dburl
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -19,13 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a%q=8fzyx01zq@fm10(5)5ccau3+cbg7w_8!f!u7$dygvgi9v1'
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['weblog-technology.herokuapp.com']
 
 
 # Application definition
@@ -78,11 +76,9 @@ WSGI_APPLICATION = 'web_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
 
 
@@ -123,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
